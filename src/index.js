6e90b7a61c08b4da7dd453deaf016ed516fa8b1b
Esteban Salazar Zapata <esteban.salazar@Esteban-Salazar-MacBook-Pro.local>
2022-09-05 20:57:06 -0500

    feat(project): implement routes for account

--- src/index.js
+++ src/index.js
@@ -1,38 +1,15 @@
-// import { createServer } from 'http';
 import express from 'express';
+import dotenv from 'dotenv';
+import accountRouter from './routes/account.js';
 
-console.clear();
+dotenv.config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const expressApp = express();
 
 expressApp.use(express.json());
 expressApp.use(express.text());
 
-// Exist a method for every method but also exist the all method to handle all the methods in a function
-// example: expressApp.all('/mi-account', (req, res) => {
-// expressApp.get('/mi-account', (req, res) => {
-//   res.send('My personal account');
-// });
-expressApp.get('/item/:iditem', (req, res) => {
-  // console.log(req.params.iditem);
-  console.log(req.headers);
-  console.log(req.get('Content-Type'));
-  // res.status(401).send({
-  //   errorMessage: 'Unauthorized access',
-  // });
-  res.send(`The item has id ${req.params.iditem}`);
-});
-
-expressApp.post('/item/:iditem', (req, res) => {
-  console.log(req.body);
-  console.log(req.query);
-  res.send(`POST - The item has id ${req.params.iditem}`);
-});
-
-expressApp.put('/product', (req, res) => {
-  console.log(req.body);
-  res.send(`PUT - Product`);
-});
+expressApp.use(accountRouter);
 
 expressApp.listen(PORT, () => console.log(`Server listening at port ${PORT}`));
---
+++ src/routes/account.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import { USERS_DB } from '../ddbb.js';
+
+const accountRouter = express.Router();
+
+accountRouter.use((req, res, next) => {
+  console.log(req.ip);
+
+  next();
+});
+
+// Get details of an account
+accountRouter.get('/account/:guid', (req, res) => {
+  const { guid } = req.params;
+  const account = USERS_DB.find((user) => user.guid === guid);
+  if (!account) return res.status(404).send('No account found');
+  return res.send(account);
+});
+// Create a new account with guid and name
+accountRouter.post('/account', (req, res) => {
+  const { guid, name } = req.body;
+  const accountIndex = USERS_DB.findIndex((user) => user.guid === guid);
+  if (accountIndex !== -1)
+    return res.status(409).send('Account already exists');
+  if (!name || !guid) return res.status(400).send('Name and guid are required');
+
+  USERS_DB.push({ guid, name });
+  return res.send('Account created successfully');
+});
+
+// Update the name of an account
+accountRouter.patch('/account/:guid', (req, res) => {
+  const { guid } = req.params;
+  const { name } = req.body;
+  const accountIndex = USERS_DB.findIndex((user) => user.guid === guid);
+  if (accountIndex === -1) return res.status(404).send('No account found');
+  if (!name) return res.status(400).send('Name is required');
+
+  USERS_DB[accountIndex].name = name;
+  return res.send('Account updated successfully');
+});
+
+// Delete an account
+accountRouter.delete('/account/:guid', (req, res) => {
+  const { guid } = req.params;
+  const accountIndex = USERS_DB.findIndex((user) => user.guid === guid);
+  if (accountIndex === -1) return res.status(404).send('No account found');
+
+  USERS_DB.splice(accountIndex, 1);
+  return res.send('Account deleted successfully');
+});
+
+export default accountRouter;
